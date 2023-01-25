0b250f3cc72c605672934db83e98d1735943c68e
정재훈 <wogns8123@naver.com>
2023-01-25 14:35:16 +0900

    center

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <nav>
-    <div id="navCategory">
+    <div>
       <router-link to="/">홈로고</router-link> &nbsp;&nbsp;&nbsp;
       <router-link to="/KidBoard">고민게시판</router-link>&nbsp;&nbsp;&nbsp;
       <router-link to="/">HELP DESK</router-link>&nbsp;&nbsp;&nbsp;
-      <router-link to="/calendar">상담관리</router-link>
-
+      <router-link to="/calendar">일정관리</router-link>&nbsp;&nbsp;&nbsp;
+      <router-link to="/clientmanage">고객관리</router-link>&nbsp;&nbsp;&nbsp;
     </div>
+    
     <div id="navMypage">
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Login</router-link> | 
       <router-link to="/">마이 페이지</router-link>
     </div>
   </nav>
@@ -32,18 +33,16 @@ nav {
   /* 반응형 x */
   width: 1200px;
   margin: 0 auto;
-
   padding: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   /* background-color: #F1E7DB; */
-  position: fixed;
+  position: relative;
   z-index: 3;
   top: 0;
   left: 0;
   right: 0;
-
 }
 
 nav a {
@@ -55,4 +54,12 @@ nav a {
 nav a.router-link-exact-active {
   color: #579BB1;
 }
+
+figuercaption{
+  display:none;
+  border:1px solid black;
+  width:150px; height:50px;
+  text-align:center;
+}
+
 </style>
