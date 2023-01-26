573c666a847dd3b08392ae4f36bd7a3ba280e564
정재훈 <wogns8123@naver.com>
2023-01-26 16:33:05 +0900

    password detail

--- frontend/src/components/account/signup/client/SignUpClient.vue
+++ frontend/src/components/account/signup/client/SignUpClient.vue
@@ -21,21 +21,28 @@
                 label="Password"
                 type="text"
                 v-model="password"
+                @blur="passwordValid"
                 required
               ></v-text-field>
             </v-col>
+            <div v-if="!passwordValidFlag">
+              유효하지않은 비밀번호입니다.              
+            </div>
             <v-col cols="12">
               <v-text-field
-                label="RE   Password"
-                type="text"
-                v-model="password2"
-                required
+              label="RE Password"
+              type="text"
+              v-model="password2"
+              @blur="passwordCheckValid"
+              required
               ></v-text-field>
             </v-col>
-
+            <div v-if="!passwordCheckFlag">
+              비밀번호가 동일하지 않습니다.           
+            </div>
             <v-col cols="12">
               <v-text-field
-                label="Name"
+              label="Name"
                 type="text"
                 v-model="name"
                 required
@@ -96,10 +103,28 @@ export default {
         name:null,
         email:null,
         number:null,
+        passwordValidFlag: true,
+        passwordCheckFlag: true,
       }
     },
 
     methods:{
+      //비밀번호 유효성 검사 
+      passwordValid(){
+        if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,16}$/.test(this.password)) {
+          this.passwordValidFlag = true
+        }else{
+          this.passwordValidFlag = false
+        }
+      },
+      passwordCheckValid(){
+        if (this.password === this.password2){
+          this.passwordCheckFlag = true
+        }else{
+          this.passwordCheckFlag = false
+        }
+      },
+      // 취소버튼 뒤로 가기
       moveBack(){
         this.$router.push({ name: 'login' })
       },
