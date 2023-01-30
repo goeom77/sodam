4b6395037b41f9007fd27d0cf42b6a73f9805ce2
Jung <wogns8123@naver.com>
2023-01-30 23:48:05 +0900

    Edit Login

--- frontend/src/components/account/login/loginDetail.vue
+++ frontend/src/components/account/login/loginDetail.vue
@@ -1,34 +1,70 @@
 <template>
-  <div >
-    <!-- 로그인 창  -->
-    <div class="loginform">
-      <div class="container__form container--signup" style="margin: 0 auto; width:455px;">
-      <form action="#" class="form" id="form1">
-        <h2 class="form__title">Login</h2>
-        <input type="radio" name="gender" v-model="common_code" common_code="2">고객
-        <input type="radio" name="gender" v-model="common_code" common_code="1">상담사
-        <br>
-        <input type="text" v-model="ID"  placeholder="ID"/>
-        <br>
-        <input type="password" v-model="password" placeholder="Password"/>
-        <br>
-        <router-link class="find" :to="{name:'findId'}">아이디찾기</router-link>
-        <router-link class="find" :to="{name:'findPw'}">비밀번호 찾기</router-link>
-        <br>
 
-        <v-btn @click="logIn">로그인</v-btn>
-        <br>
-        <br>
-        <LoginKakao/>
+  <v-tabs>
+    <v-tab @click="ChangeToClient">
+      고객
+    </v-tab>
+    <v-tab @click="ChangeToCounselor">
+      상담사
+    </v-tab>
+  </v-tabs>
 
-        <div style="display:block">
-          <h4>계정이 없으신가요?</h4>
-          <router-link class="find" :to="{name:'signupclient'}">회원가입</router-link>
-        </div>
-      </form>
-    </div>
-  </div>
-  </div>
+  <v-tabs-items v-model="tab">
+    <v-tab-item>
+          <div v-if="this.common_code===2">
+            고객 로그인 창 
+            <div class="loginform">
+              <div class="container__form container--signup" style="margin: 0 auto; width:455px;">
+              <form action="#" class="form" id="form1">
+                <h2 class="form__title">Login</h2>
+                <br>
+                <input type="text" v-model="ID"  placeholder="ID"/>
+                <br>
+                <input type="password" v-model="password" placeholder="Password"/>
+                <br>
+                <router-link class="find" :to="{name:'findId'}">아이디찾기</router-link>
+                <router-link class="find" :to="{name:'findPw'}">비밀번호 찾기</router-link>
+                <br>
+                <v-btn @click="logIn">로그인</v-btn>
+                <br>
+                <br>
+                <LoginKakao/>
+                <div style="display:block">
+                  <h4>계정이 없으신가요?</h4>
+                  <router-link class="find" :to="{name:'signupclient'}">회원가입</router-link>
+                </div>
+              </form>
+            </div>
+          </div>
+          </div>
+
+          <div v-if="this.common_code===1">
+            상담사 로그인 창 
+            <div class="loginform">
+              <div class="container__form container--signup" style="margin: 0 auto; width:455px;">
+              <form action="#" class="form" id="form1">
+                <h2 class="form__title">Login</h2>
+                <input type="text" v-model="ID"  placeholder="ID"/>
+                <br>
+                <input type="password" v-model="password" placeholder="Password"/>
+                <br>
+                <router-link class="find" :to="{name:'findId'}">아이디찾기</router-link>
+                <router-link class="find" :to="{name:'findPw'}">비밀번호 찾기</router-link>
+                <br>
+                <v-btn @click="logIn">로그인</v-btn>
+                <br>
+                <br>
+                <LoginKakao/>
+                <div style="display:block">
+                  <h4>계정이 없으신가요?</h4>
+                  <router-link class="find" :to="{name:'signupcounselor'}">회원가입</router-link>
+                </div>
+              </form>
+            </div>
+          </div>
+          </div>
+    </v-tab-item>
+  </v-tabs-items>
 </template>
 
 <script>
@@ -56,6 +92,13 @@ export default {
         console.log(payload)
         this.$store.dispatch('login', payload)
       },
+      ChangeToClient(){
+        this.common_code=2
+      },
+      ChangeToCounselor(){
+        this.common_code=1
+      }
+
       // moveFindId(){
       //   console.log('파인드 아이디 입구')
       //   this.$router.push({name:'find_Id'})
