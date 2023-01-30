30913ee8951ff481bc7beecee6a2a829345d3654
Jung <wogns8123@naver.com>
2023-01-31 01:00:34 +0900

    kakao...

--- frontend/src/components/account/login/LoginKakao.vue
+++ frontend/src/components/account/login/LoginKakao.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <button class="background2" @click="loginWithKakao"></button>
+    <a id="custom-login-btn" @click="kakaoLogin()">
+      <img
+      src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+      width="222"
+      alt="카카오 로그인 버튼"
+      />
+    </a>
+    <div @click="kakaoLogout()">로그아웃</div>
   </div>
-  
+  <!-- <div>
+    <button class="background2" @click="loginWithKakao"></button>
+  </div> -->
 <!-- 
 
   <a id="loginBtn" type="button" onclick="function login() {
@@ -15,18 +24,54 @@ login()"><img src="kakao_login_btn.png" alt="button"></a>
 </template>
 
 <script>
+const API_URL = 'http://127.0.0.1:8080'
+
 export default {
-    name: "LoginKakao",
-    // methods: {
-    //     loginWithKakao() {
-    //       console.log('가나다라')
-    //       const params = {
-    //           redirectUri: "http://localhost:80",
-    //       };
-    //       window.Kakao.Auth.authorize(params);
-    //     },
-    // },
+  methods: {
+    kakaoLogin() {
+      window.Kakao.Auth.login({
+        scope: "profile_nickname",
+        success: this.getKakaoAccount,
+      });
+    },
+    getKakaoAccount() {
+      window.Kakao.API.request({
+        url: `${API_URL}/api/auth/kakao`,
+        success: (res) => {
+          const kakao_account = res.kakao_account;
+          const ninkname = kakao_account.profile.ninkname;
+          const email = kakao_account.email;
+          console.log("ninkname", ninkname);
+          console.log("email", email);
+
+          //로그인처리구현
+
+          alert("로그인 성공!");
+        },
+        fail: (error) => {
+          console.log(error);
+        },
+      });
+    },
+    kakaoLogout() {
+      window.Kakao.Auth.logout((res) => {
+        console.log(res);
+      });
+    },
+  },
 };
+// export default {
+//     name: "LoginKakao",
+//     methods: {
+//         loginWithKakao() {
+//           console.log('가나다라')
+//           const params = {
+//               redirectUri: "http://localhost:80",
+//           };
+//           window.Kakao.Auth.authorize(params);
+//         },
+//     },
+// };
 </script>
 
 <style>
