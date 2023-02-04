65cb0e506b23fa0028ca03573b266e0346ea62dc
hyun132 <rlaalgus132@naver.com>
2023-02-04 19:57:04 +0900

    상담목록 api 파라미터 수정

--- backend/src/main/java/com/samsung/sodam/api/request/ReviewRequest.java
+++ backend/src/main/java/com/samsung/sodam/api/request/ReviewRequest.java
@@ -1,5 +1,6 @@
 package com.samsung.sodam.api.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -11,7 +12,10 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ReviewRequest {
     //최신순, 별점순
-    String type;
-    String clientId;
+    @ApiModelProperty(value="상담 타입", example="EDUCATION",required = false)
+    String type="ALL";
+    @ApiModelProperty(value="고객 아이디", example="clientId",required = false)
+    String clientId="";
+    @ApiModelProperty(value="상담사 아이디", example="counselorId",required = true)
     String counselorId;
 }
--- backend/src/main/java/com/samsung/sodam/db/repository/ReviewCustomRepositoryImpl.java
+++ backend/src/main/java/com/samsung/sodam/db/repository/ReviewCustomRepositoryImpl.java
@@ -2,34 +2,55 @@ package com.samsung.sodam.db.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.samsung.sodam.api.request.ReviewRequest;
-import com.samsung.sodam.db.entity.QReview;
 import com.samsung.sodam.db.entity.Review;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.samsung.sodam.db.entity.QReview.review;
 
 @Repository
 public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
-    private JPAQueryFactory queryFactory;
+    public final JPAQueryFactory queryFactory;
+
+    public ReviewCustomRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
 
     @Override
     public Page<Review> getAllMyReview(ReviewRequest request, Pageable pageable) {
-        List<Review> list = queryFactory.selectFrom(review)
-                .where(review.counselorId.eq(request.getCounselorId())).fetch();
+        List<Review> list = new ArrayList<>();
+        if (checkIsNullOrEmpty(request.getClientId()) && (checkIsNullOrEmpty(request.getType()) || request.getType().equals("ALL"))) {
+            list = queryFactory.selectFrom(review)
+                    .where(review.counselorId.eq(request.getCounselorId())).fetch();
+            return new PageImpl<>(list, pageable, list.size());
+        } else if (checkIsNullOrEmpty(request.getClientId())) {
 
-        return new PageImpl<>(list, pageable, list.size());
+            list = queryFactory.selectFrom(review)
+                    .where(review.counselorId.eq(request.getCounselorId()),review.type.like(request.getType())).fetch();
+            return new PageImpl<>(list, pageable, list.size());
+        } else if (checkIsNullOrEmpty(request.getType()) || request.getType().equals("ALL")) {
+            list = queryFactory.selectFrom(review)
+                    .where(review.counselorId.eq(request.getCounselorId()),review.clientId.like(request.getClientId())).fetch();
+            return new PageImpl<>(list, pageable, list.size());
+        }
+
+
+        return new PageImpl<>(list, pageable, 0);
     }
 
     @Override
     public List<Review> getMyReview(String clientId) {
         return queryFactory.selectFrom(review)
                 .where(review.clientId.eq(clientId)).fetch();
+    }
+
+    public boolean checkIsNullOrEmpty(String input) {
+        return input == null || input.isEmpty() || input.isBlank();
     }
 }
