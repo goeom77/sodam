e57903e7b1b570607dbd7498c64822c19b4c298e
mauv2sky <mauv2sky@gmail.com>
2023-02-06 22:51:27 +0900

    Update. 알림데이터 JSON 처리

--- backend/src/main/java/com/samsung/sodam/api/controller/NotificationController.java
+++ backend/src/main/java/com/samsung/sodam/api/controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.samsung.sodam.api.controller;
 
 import com.samsung.sodam.api.service.NotificationService;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class NotificationController {
         this.notificationService = notificationService;
     }
 
-    @GetMapping(value = "/subscription", produces = "text/event-stream")
+    @GetMapping(value = "/subscription", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @ApiOperation(value = "알림 구독", notes = "로그인한 유저는 알림 구독을 요청한다.")
     public SseEmitter subscribe(@AuthenticationPrincipal UserDetails userDetails,
                                 @RequestParam(value = "lastEventId", required = false, defaultValue = "") String lastEventId) {
