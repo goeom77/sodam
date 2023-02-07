a37cc6f8140f619ee19c6f506ddb7b40c5d0e2c5
wogns8123 <wogns8123@naver.com>
2023-02-08 01:38:51 +0900

    review update

--- frontend/src/components/ConsultantCard/ConsultantCardItemReviewItemDetail.vue
+++ frontend/src/components/ConsultantCard/ConsultantCardItemReviewItemDetail.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    {{ id }}
-    {{ reviewData }}
+    {{ this.reviewData }}
+    <br>
+    상담 분야 : {{ this.reviewData.type }}
+    <br>
+    평점 : {{ this.reviewData.stars }}
+    <br>
+    제목 : {{ this.reviewData.title }}
+    <br>
+    내용 : {{ this.reviewData.contents }}
+    <br>
+    작성일시 : {{ this.reviewData.createdAt }}
+    <br>
+    조회수 : {{ this.reviewData.pastCount }}
+    <br>
+    
+    <v-btn outlined rounded text @click="ReviewUpdate">수정</v-btn>
+    <v-btn outlined rounded text @click="ReviewDelete">삭제</v-btn>
   </div>
 </template>
 
@@ -14,6 +29,11 @@ export default {
     props:{
       id: String
     },
+    data(){
+      return{
+        reviewData:null,
+      }
+    },
     methods:{
       getReviewInfo(){
         axios({
@@ -21,15 +41,21 @@ export default {
           url:`${VUE_APP_API_URL}/api/review/search?id=${this.id}`,
           data: {
             id: this.id,
-            reviewData:null,
           },
           headers: {
             Authorization : `Bearer ${this.$store.state.token.token.access_token}`
           }
         })
         .then(res=>{
+          console.log(res.data)
           this.reviewData = res.data
         })
+      },
+      ReviewUpdate(){
+        this.$router.push({ name: 'ConsultantCardItemReviewUpdate', params: {id: this.id}  })
+      },
+      ReviewDelete(){
+
       }
     },
     created(){
