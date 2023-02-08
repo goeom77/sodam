8d732cbbe060b3c8424d444416eb62acbf423109
정재훈 <wogns8123@naver.com>
2023-02-08 17:59:49 +0900

    f calendar

--- frontend/src/components/management/schedule/CalendarView.vue
+++ frontend/src/components/management/schedule/CalendarView.vue
@@ -1,46 +1,45 @@
-<template>
-  <div>
-    <div class="calendar">
-      <h1>일정</h1>
-      <FullCalendar :options="calendarOptions" />
-    </div>
-  </div>
-</template>
-
 <script>
+import { defineComponent } from 'vue'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import {INITIAL_EVENTS, createEventId} from './event-utils'
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 
-export default {
-  name:'CalendarView',
+export default defineComponent({
   components: {
-    FullCalendar // make the <FullCalendar> tag available
+    FullCalendar,
   },
   data() {
     return {
       calendarOptions: {
-        plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth',
-        initialEvents: INITIAL_EVENTS,
-        headerToolbar:{
-          left: 'prev, next today',
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin // needed for dateClick
+        ],
+        headerToolbar: {
+          left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth, timeGridWeek, timeGridDay'
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
+        initialView: 'dayGridMonth',
+        initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
         editable: true,
         selectable: true,
-        selectMirror: false,
+        selectMirror: true,
         dayMaxEvents: true,
         weekends: true,
         select: this.handleDateSelect,
         eventClick: this.handleEventClick,
-        eventsSet: this.handleEvents,
-        locale:'ko',
+        eventsSet: this.handleEvents
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
       },
-      currentEvent:['가나다라'],
+      currentEvents: [],
     }
   },
   methods: {
@@ -50,7 +49,9 @@ export default {
     handleDateSelect(selectInfo) {
       let title = prompt('상담 일정을 입력해주세요')
       let calendarApi = selectInfo.view.calendar
+
       calendarApi.unselect() // clear date selection
+
       if (title) {
         calendarApi.addEvent({
           id: createEventId(),
@@ -59,8 +60,9 @@ export default {
           end: selectInfo.endStr,
           allDay: selectInfo.allDay
         })
-        console.log(selectInfo)
       }
+      
+      console.log(selectInfo)
     },
     handleEventClick(clickInfo) {
       if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
@@ -71,15 +73,101 @@ export default {
       this.currentEvents = events
     },
   }
-
-}
+})
 
 </script>
 
-<style>
-.calendar{
-  margin:50px;
-  
+<template>
+  <div class='demo-app'>
+    <div class='demo-app-sidebar'>
+      <div class='demo-app-sidebar-section'>
+        <label>
+          <input
+            type='checkbox'
+            :checked='calendarOptions.weekends'
+            @change='handleWeekendsToggle'
+          />
+          toggle weekends
+        </label>
+      </div>
+      <div class='demo-app-sidebar-section'>
+        <h2>All Events ({{ currentEvents.length }})</h2>
+        <ul>
+          <li v-for='event in currentEvents' :key='event.id'>
+            <b>{{ event.startStr }}</b>
+            <br>
+            <i>{{ event.title }}</i>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class='demo-app-main'>
+      <FullCalendar
+        class='demo-app-calendar'
+        :options='calendarOptions'
+      >
+        <template v-slot:eventContent='arg'>
+          <b>{{ arg.timeText }}</b>
+          <i>{{ arg.event.title }}</i>
+        </template>
+      </FullCalendar>
+    </div>
+  </div>
+</template>
 
+<style lang='css'>
+
+h2 {
+  margin: 0;
+  font-size: 16px;
 }
+
+ul {
+  margin: 0;
+  padding: 0 0 0 1.5em;
+}
+
+li {
+  margin: 1.5em 0;
+  padding: 0;
+}
+
+b { /* used for event dates/times */
+  margin-right: 3px;
+}
+
+.demo-app {
+  display: flex;
+  min-height: 100%;
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-sidebar {
+  width: 300px;
+  line-height: 1.5;
+  background: #eaf9ff;
+  border-right: 1px solid #d3e2e8;
+}
+
+.demo-app-sidebar-section {
+  padding: 2em;
+}
+
+.demo-app-main {
+  flex-grow: 1;
+  padding: 3em;
+}
+
+.fc { /* the calendar root */
+  max-width: 1100px;
+  margin: 0 auto;
+}
+.fc-daygrid-day-number{
+  color: black;
+}
+.fc-day-number.fc-sat.fc-past { color:#0000FF; }     /* 토요일 */
+.fc-day-number.fc-sun.fc-past { color:#FF0000; }    /* 일요일 */
+
+
 </style>
