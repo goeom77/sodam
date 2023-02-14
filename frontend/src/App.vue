225302cf51b50944d70434ebd72c5be70e2763f3
정재훈 <wogns8123@naver.com>
2023-02-14 18:14:38 +0900

    last
    Former-commit-id: 73db25031eaf1030d8d9d0c341160fa409bf3eec

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,7 +32,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-person" viewBox="0 0 16 16">
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
             </svg>
-            <button @click="logOut"><p class="mt-1">마이페이지</p></button>
+            <button ><p class="mt-1">마이페이지</p></button>
           </router-link>
         </div>
         <!-- 로그인 상태 끝 -->
@@ -127,13 +127,16 @@ export default {
     logOut(){
       axios({
         method: 'get',
-        url: `${VUE_APP_API_URL}/logout/id`,
+        url: `${VUE_APP_API_URL}/api/auth/logout`,
         headers: {
           Authorization : `Bearer ${this.$store.state.token.token.access_token}`
         }
       })
-      .then(
+      .then(res=>{
         this.$store.dispatch('logOut')
+        this.$router.push({name:'home'})
+      })
+      .then(
       )
     },
     logIn(){
