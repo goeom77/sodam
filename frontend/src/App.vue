bd59a77a28ccc5ac8a18657f394d89ff5d12327b
goeom77 <7eom14@gmail.com>
2023-02-17 06:04:12 +0900

    feat app nav edit
    Former-commit-id: 91e1e728f9ebb6a11ced9ee880328684757dc2bb

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -107,9 +107,9 @@
           <div style="height:220px"></div>
           <ul>
             <li><router-link to="/BoardView">고민게시판</router-link>&nbsp;&nbsp;&nbsp;</li>
-            <li><router-link to="/Calendar">일정관리</router-link>&nbsp;&nbsp;&nbsp;</li>
+            <li v-if="common_code === '1'"><router-link to="/Calendar">일정관리</router-link>&nbsp;&nbsp;&nbsp;</li>
             <li v-if="common_code === '1'"><router-link to="/ClientManage">고객관리</router-link>&nbsp;&nbsp;&nbsp;</li>
-            <li v-if="common_code === '1'"><router-link to="/HelpView">HELP DESK</router-link>&nbsp;&nbsp;&nbsp;</li>
+            <li><router-link to="/HelpView">HELP DESK</router-link>&nbsp;&nbsp;&nbsp;</li>
           </ul>
           <div style="height:220px"></div>
         </nav>
