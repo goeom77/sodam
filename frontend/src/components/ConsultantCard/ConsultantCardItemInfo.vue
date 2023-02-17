ce15cc871039d29bcb3a84bb490fe56cbd2011a7
정재훈 <wogns8123@naver.com>
2023-02-17 11:39:10 +0900

    check
    Former-commit-id: 4d47ea4e22bbdbf7dd0dc2a3ca27c03ba0bb60ba

--- frontend/src/components/ConsultantCard/ConsultantCardItemInfo.vue
+++ frontend/src/components/ConsultantCard/ConsultantCardItemInfo.vue
@@ -6,7 +6,7 @@
         <img class="detailProfileImg" v-else v-bind:src="`${this.nullProfileImg}`" alt="까비" >
       </div>
       <div class="child2" style="padding-left:30px">
-        <div v-if:toggleHeart="!toggleHeart" @click="likeCounselor">
+        <div  v-if="this.likestatus===false" @click="likeCounselor">
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16" style="float:right; cursor:pointer;">
             <path fill-rule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
             <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
@@ -62,8 +62,8 @@ export default {
     return{
       nullProfileImg : require('../../assets/images/사람altimg.png'),
       common_code: this.$store.state.payload.common_code,
-      clientId:this.$store.state.payload.id,
-      toggleHeart: this.$store.state.userSignupData.like
+      clientId:this.$store.state.payload.id,  
+      likestatus:false
     }
   },
   methods:{
@@ -80,8 +80,7 @@ export default {
         }
       })
       .then(res=>{
-        this.toggleHeart = true;
-        this.$store.state.userSignupData.like = true;
+        this.likestatus = true;
       })
     },
     deleteFavCounselor() {
@@ -97,8 +96,7 @@ export default {
         }
       })
       .then(res=>{
-        this.toggleHeart = false;
-        this.$store.state.userSignupData.like = false;
+        this.likestatus = false;
       })
     },
     convertConsultType(list) {
