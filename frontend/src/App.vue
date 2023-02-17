dd5c57a5afbcc497d8901030907a46968018f0a6
hyun <rlaalgus132@naver.com>
2023-02-17 14:32:50 +0900

    Revert "화면 겹치는 문제"
    This reverts commit 8f63345462e0a4f6064d7559bb128fcc56cc8be7 [formerly e1b47402231c5c027c93c55de93d96cc8947fc83].
    
    
    Former-commit-id: 4231146a3a3f8fe6601b6e2d661d769fbf4746da

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div
-      class="mr-5"
-      style="position: fixed; z-index: 10; top: 20px; right:5px;"
+    class="mr-5"
+    style="position: fixed; z-index: 10; top: 20px; right:5px;"
   >
     <v-btn
-        v-if="isLogin=== false"
-        @click="dialogclick"
-        class="ma-2"
-        color="purple"
-        icon="mdi-account-outline"
+      v-if="isLogin=== false"
+      @click="dialogclick"
+      class="ma-2"
+      color="purple"
+      icon="mdi-account-outline"
     ></v-btn>
     <v-btn
-        v-else
-        @click="dialogclick"
-        class="ma-2"
-        color="indigo"
-        icon="mdi-account-outline"
+      v-else
+      @click="dialogclick"
+      class="ma-2"
+      color="indigo"
+      icon="mdi-account-outline"
     ></v-btn>
   </div>
   <v-card
-      id="myCard"
-      v-if="dialog===true"
-      class="mx-auto bg-light"
-      width="300"
-      style="position: fixed; z-index: 9; top: 60px; right:50px;"
-      @blur="dialogclick"
+    id="myCard"
+    v-if="dialog===true"
+    class="mx-auto bg-light"
+    width="300"
+    style="position: fixed; z-index: 9; top: 60px; right:50px;"
+    @blur="dialogclick"
   >
     <v-card-item>
       <div style="text-align: start;">
@@ -34,20 +34,18 @@
             {{ this.$store.state.payload.id }}님
             <span id="navMypage">
               <!-- 알람 -->
-              <v-btn class="text-none" variant="plain">
+              <v-btn class="text-none" variant="plain" >
                 <v-badge :content="newNotiCount" color="error" @click="alarm">
                   <v-icon variant="text">mdi-bell-outline</v-icon>
                 </v-badge>
               </v-btn>
             </span>
-          </div>
+            </div>
           <hr>
           <!-- 로그아웃 -->
           <div class="ml-5">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                 class="bi bi-door-open-fill" viewBox="0 0 16 16" @click="logOut" type="button">
-              <path
-                  d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+            <svg xmlns="http://www.w3.org/2000/svg"  width="30" height="30" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16" @click="logOut" type="button">
+              <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
             </svg>
             <button @click="logOut" class="ml-5"><p class="mt-1">로그아웃</p></button>
           </div>
@@ -59,18 +57,15 @@
           </router-link>
           <hr>
           <!-- 마이페이지 -->
-          <router-link to="/mypage" style="text-decoration: none ;" class="ml-5" type="button" @click="dialogoff">
+          <router-link to="/mypage" style="text-decoration: none ;"  class="ml-5" type="button" @click="dialogoff">
             <v-icon variant="text" size="x-large">mdi-account-cog-outline</v-icon>
             <button><p class="mt-1 ml-5">마이페이지</p></button>
           </router-link>
           <hr>
-          <!-- 개인정보 수정 -->
-          <router-link to="/CheckEditInformation" style="text-decoration: none ;" class="ml-5" type="button"
-                       @click="dialogoff">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-person"
-                 viewBox="0 0 16 16">
-              <path
-                  d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+           <!-- 개인정보 수정 -->
+          <router-link to="/CheckEditInformation" style="text-decoration: none ;"  class="ml-5" type="button" @click="dialogoff">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-person" viewBox="0 0 16 16">
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
             </svg>
             <button><p class="mt-1  ml-5">개인정보 수정</p></button>
           </router-link>
@@ -78,12 +73,10 @@
         <!-- 로그인 상태 끝 -->
         <!-- 로그 아웃상태 -->
         <div v-if="isLogin===false" class="mt-3">
-          <router-link to="/login" style="text-decoration: none ;" class="ml-5" type="button" @click="dialogoff">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-door-open"
-                 viewBox="0 0 16 16" @click="logIn">
+          <router-link to="/login" style="text-decoration: none ;"  class="ml-5" type="button" @click="dialogoff">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16" @click="logIn">
               <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
-              <path
-                  d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
+              <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
             </svg>
             <button @click="logIn" class="ml-5"><p class="mt-1">로그인</p></button>
           </router-link>
@@ -98,51 +91,35 @@
       </v-btn>
     </v-card-actions>
   </v-card>
-  <v-container id="fh5co-page">
-    <v-container id="fh5co-aside" role="complementary"
-                 class="border js-fullheight d-flex justify-center align-center mb-6 bg-surface-variant"
-                 style="width: 15%">
-      <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-      <v-container>
-        <nav id="fh5co-main-menu" role="navigation" class="mx-auto">
+  <v-container id="fh5co-page" >
+      <v-container id="fh5co-aside" role="complementary" class="border js-fullheight d-flex justify-center align-center mb-6 bg-surface-variant" style="width: 15%" >
+        <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+        <nav id="fh5co-main-menu" role="navigation">
 
-          <div style="height:220px">
-          </div>
-          <router-link to="/">
+          <router-link to="/"  >
             <img
-                id="logo"
-                class="align-self-center h-auto w-50"
-                :src="projectlogo"
-                alt="noimage"
+              id="logo"
+              class="align-self-center h-auto w-50"
+              :src="projectlogo"
+              alt="noimage"
             />
           </router-link>
-          <div style="height:20px">
-
-          </div>
-          <ul >
-            <li>
-              <router-link to="/BoardView">고민게시판</router-link>&nbsp;&nbsp;&nbsp;
-            </li>
-            <li v-if="common_code == '1'">
-              <router-link to="/Calendar">일정관리</router-link>&nbsp;&nbsp;&nbsp;
-            </li>
-            <li v-if="common_code == '1'">
-              <router-link to="/ClientManage">고객관리</router-link>&nbsp;&nbsp;&nbsp;
-            </li>
-            <li>
-              <router-link to="/HelpView">HELP DESK</router-link>&nbsp;&nbsp;&nbsp;
-            </li>
+          <div style="height:220px"></div>
+          <ul>
+            <li><router-link to="/BoardView">고민게시판</router-link>&nbsp;&nbsp;&nbsp;</li>
+            <li v-if="common_code == '1'"><router-link to="/Calendar">일정관리</router-link>&nbsp;&nbsp;&nbsp;</li>
+            <li v-if="common_code == '1'"><router-link to="/ClientManage">고객관리</router-link>&nbsp;&nbsp;&nbsp;</li>
+            <li><router-link to="/HelpView">HELP DESK</router-link>&nbsp;&nbsp;&nbsp;</li>
           </ul>
           <div style="height:220px"></div>
         </nav>
       </v-container>
-    </v-container>
-    <!--      <v-container id="fh5co-main" style="width: 100%; padding: 0; position: fixed;left: 15%; top: 0;">-->
-    <v-container>
-      <router-view/>
-    </v-container>
+<!--      <v-container id="fh5co-main" style="width: 100%; padding: 0; position: fixed;left: 15%; top: 0;">-->
+      <v-container>
+        <router-view />
+      </v-container>
   </v-container>
-
+  
   <!-- <LoadingView :loading="loadingStatus"></LoadingView> -->
   <!-- <LoadingView :loading="this.$store.state.loadingStatus"></LoadingView> -->
 </template>
@@ -150,22 +127,22 @@
 <script>
 import axios from 'axios'
 // import axios from '@/store/instance.js'
-import {EventSourcePolyfill} from "event-source-polyfill";
+import { EventSourcePolyfill } from "event-source-polyfill";
 // import LoadingView from '../src/views/common/LoadingView'
 
-document.querySelector('body').setAttribute('style', "margin: 0;")
+document.querySelector('body').setAttribute('style',"margin: 0;")
 const VUE_APP_API_URL = process.env.VUE_APP_API_URL
 const LOCAL_URL = process.env.LOCAL_URL
 const VUE_APP_STT_API_URL = process.env.VUE_APP_STT_API_URL
 
 export default {
-  name: 'App',
-  data() {
-    return {
+  name:'App',
+  data(){
+    return{
       common_code: this.$store.state.payload.common_code,
-      dialog: false,
-      projectlogo: require('../src/assets/images/projectlogoperpect.png'),
-      newNotiCount: this.$store.state.newNotiCount,
+      dialog : false,
+      projectlogo : require('../src/assets/images/projectlogoperpect.png'),
+      newNotiCount : this.$store.state.newNotiCount,
     }
   },
   component: {
@@ -190,46 +167,46 @@ export default {
     //     }
     //   })
     // },
-    logOut() {
+    logOut(){
       axios({
         method: 'get',
         url: `${VUE_APP_API_URL}/api/auth/logout`,
         headers: {
-          Authorization: `Bearer ${this.$store.state.token.token.access_token}`
+          Authorization : `Bearer ${this.$store.state.token.token.access_token}`
         }
       })
-          .then(res => {
-            this.dialogoff();
-            this.$store.dispatch('logOut')
-            this.$router.push({name: 'home'})
-          })
-          .then(
-          )
+      .then(res=>{
+        this.dialogoff();
+        this.$store.dispatch('logOut')
+        this.$router.push({name:'home'})
+      })
+      .then(
+      )
     },
-    logIn() {
+    logIn(){
       this.dialogoff();
-      this.$router.push({name: 'login'});
+      this.$router.push({name:'login'});
     },
-    alarm() {
+    alarm(){
       this.dialogoff();
-      this.$router.push({name: 'AlarmView'});
+      this.$router.push({name:'AlarmView'});
     },
     initNotiListener() {
       const eventSource = new EventSourcePolyfill(`${VUE_APP_API_URL}/api/subscription`, {
         headers: {
-          "Authorization": `Bearer ${this.$store.state.token.token.access_token}`
+          "Authorization" : `Bearer ${this.$store.state.token.token.access_token}`
         },
-        withCredentials: true,
-        heartbeatTimeout: 86400000000
+        withCredentials : true,
+        heartbeatTimeout : 86400000000
       });
 
       // 알림 권한 설정
       let granted = false;
       if (Notification.permission === 'granted') {
-        granted = true;
+          granted = true;
       } else if (Notification.permission !== 'denied') {
-        Notification.requestPermission()
-            .then((permission) => granted = permission === 'granted')
+          Notification.requestPermission()
+                      .then((permission) => granted = permission === 'granted')          
       }
 
       eventSource.onopen = e => {
@@ -239,39 +216,39 @@ export default {
       eventSource.addEventListener('noti', event => {
         // 새로운 알림 수 + 1
         this.$store.dispatch('countNoti');
-
+        
         const data = JSON.parse(event.data);
-
+        
         // 브라우저 알림
         const showNotification = () => {
-          const notification = new Notification("소담", {
-            body: data.title,
-            icon: require('../src/assets/images/faviconalarm.png')
-          });
-
-          notification.addEventListener('click', () => {
-            // url: `${VUE_APP_API_URL}/api/auth/kakao`
-            window.open(`${VUE_APP_API_URL}/AlarmView`, '_self');
-          });
+            const notification = new Notification("소담", {
+                body: data.title,
+                icon: require('../src/assets/images/faviconalarm.png')
+            });
+            
+            notification.addEventListener('click', () => {
+              // url: `${VUE_APP_API_URL}/api/auth/kakao`
+                window.open(`${VUE_APP_API_URL}/AlarmView`, '_self');
+            });
         }
 
         // 알림 보여주기
         if (granted) {
-          showNotification();
+            showNotification();
         }
       });
 
       eventSource.onerror = event => {
         console.log(event);
       }
-
+      
     }
   },
-  computed: {
+  computed:{
     store() {
       return store
     },
-    isLogin() {
+    isLogin(){
       return this.$store.getters.isLogin
     },
     checkNotiCount() {
@@ -284,13 +261,13 @@ export default {
     }
   },
   beforeUpdate() {
-    if (this.$store.getters.isLogin) {
+    if(this.$store.getters.isLogin) {
       this.initNotiListener();
       this.$store.dispatch('unreadNotiCount');
     }
   },
   mounted() {
-    if (this.$store.getters.isLogin) {
+    if(this.$store.getters.isLogin) {
       this.initNotiListener();
       this.$store.dispatch('unreadNotiCount');
     }
@@ -299,14 +276,13 @@ export default {
 </script>
 
 <style>
-.fh5co-page {
+.fh5co-page{
   font-family: "Roboto", Arial, sans-serif;
   font-weight: 300;
   font-size: 20px;
   line-height: 1.6;
   color: rgba(0, 0, 0, 0.5);
 }
-
 @media screen and (max-width: 992px) {
   .fh5co-page {
     font-size: 16px;
@@ -319,12 +295,10 @@ li {
   -o-transition: 0.5s;
   transition: 0.5s;
 }
-
 li:hover, a:active, a:focus {
   color: #da1212;
   outline: none;
 }
-
 ::-webkit-selection {
   color: #fff;
   background: #da1212;
@@ -349,22 +323,19 @@ li:hover, a:active, a:focus {
   bottom: 0;
   top: 0;
   left: 0;
-  overflow: hidden;
+  overflow:hidden;
   -webkit-transition: 0.5s;
   -o-transition: 0.5s;
   transition: 0.5s;
 }
-
 #fh5co-aside.border {
   border-right: 1px solid #e6e6e6;
 }
-
 @media screen and (max-width: 1200px) {
   #fh5co-aside {
     width: 30%;
   }
 }
-
 @media screen and (max-width: 768px) {
   #fh5co-aside {
     width: 270px;
@@ -375,29 +346,24 @@ li:hover, a:active, a:focus {
     transform: translateX(-270px);
   }
 }
-
 #fh5co-aside #fh5co-logo {
   text-align: center;
 }
-
 #fh5co-aside #fh5co-main-menu ul {
   text-align: center;
   margin: 0;
   padding: 0;
 }
-
 @media screen and (max-width: 768px) {
   #fh5co-aside #fh5co-main-menu ul {
     margin: 0 0 2em 0;
   }
 }
-
 #fh5co-aside #fh5co-main-menu ul li {
   margin: 0 0 10px 0;
   padding: 0;
   list-style: none;
 }
-
 #fh5co-aside #fh5co-main-menu ul li a {
   color: rgba(0, 0, 0, 0.5);
   text-decoration: none;
@@ -413,7 +379,6 @@ li:hover, a:active, a:focus {
   letter-spacing: .2em;
   font-family: "Montserrat", Arial, sans-serif;
 }
-
 #fh5co-aside #fh5co-main-menu ul li a:after {
   content: "";
   position: absolute;
@@ -434,12 +399,10 @@ li:hover, a:active, a:focus {
   -o-transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
   transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
-
 #fh5co-aside #fh5co-main-menu ul li a:hover {
   text-decoration: none;
   color: black;
 }
-
 #fh5co-aside #fh5co-main-menu ul li a:hover:after {
   visibility: visible;
   -webkit-transform: scaleX(1);
@@ -448,11 +411,9 @@ li:hover, a:active, a:focus {
   -o-transform: scaleX(1);
   transform: scaleX(1);
 }
-
 #fh5co-aside #fh5co-main-menu ul li.fh5co-active a {
   color: black;
 }
-
 #fh5co-aside #fh5co-main-menu ul li.fh5co-active a:after {
   visibility: visible;
   -webkit-transform: scaleX(1);
@@ -466,20 +427,16 @@ li:hover, a:active, a:focus {
   cursor: pointer;
   text-decoration: none;
 }
-
 .fh5co-nav-toggle.active i::before, .fh5co-nav-toggle.active i::after {
   background: #000;
 }
-
 .fh5co-nav-toggle.dark.active i::before, .fh5co-nav-toggle.dark.active i::after {
   background: #000;
 }
-
 .fh5co-nav-toggle:hover, .fh5co-nav-toggle:focus, .fh5co-nav-toggle:active {
   outline: none;
   border-bottom: none !important;
 }
-
 .fh5co-nav-toggle i {
   position: relative;
   display: -moz-inline-stack;
@@ -495,7 +452,6 @@ li:hover, a:active, a:focus {
   background: #000;
   transition: all .2s ease-out;
 }
-
 .fh5co-nav-toggle i::before, .fh5co-nav-toggle i::after {
   content: '';
   width: 30px;
@@ -507,14 +463,12 @@ li:hover, a:active, a:focus {
   -o-transition: 0.2s;
   transition: 0.2s;
 }
-
 .fh5co-nav-toggle.dark i {
   position: relative;
   color: #000;
   background: #000;
   transition: all .2s ease-out;
 }
-
 .fh5co-nav-toggle.dark i::before, .fh5co-nav-toggle.dark i::after {
   background: #000;
   -webkit-transition: 0.2s;
@@ -573,7 +527,6 @@ li:hover, a:active, a:focus {
   -o-transition: 0.5s;
   transition: 0.5s;
 }
-
 @media screen and (max-width: 768px) {
   .fh5co-nav-toggle {
     opacity: 1;
